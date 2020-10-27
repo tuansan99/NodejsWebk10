14421cb29d71417bb11ae3fdd856772a9fdd4632
tuansan99 <73542092+tuansan99@users.noreply.github.com>
2020-10-27 22:27:36 +0700

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,8 +1,1 @@
-http.createServer(function (request, response) {
-// Send the HTTP header
-// HTTP Status: 200 : OK
-// Content Type: text/plain
-response.writeHead(200, {'Content-Type': 'text/plain'});
-// Send the response body as "Hello World"
-response.end('Hello World\n');
-}).listen(8081);
+console.log("Hello, World!")
